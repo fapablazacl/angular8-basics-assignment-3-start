6550b29891292d74b249f6463329120ceabdb42b
Felipe Apablaza <felipe.apablaza@bci.cl>
2019-11-23 11:31:09 -0300

    Asigment done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private _togglingEventLog = new Array<Date>();
+
+  private _displayParagraph = true;
+
+  public isParagraphVisible() : boolean {
+    return this._displayParagraph;
+  }
+
+  public get togglingEventLog() : Array<Date> {
+    return this._togglingEventLog;
+  }
+
+  public toggleVisible() : void {
+    this._displayParagraph = !this._displayParagraph;
+    this._togglingEventLog.push(new Date());
+
+    console.log(this._displayParagraph);
+  }
 }
